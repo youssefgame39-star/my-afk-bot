8b50b9f83d65f82e2e1b1ffb05f3d128c6550e8f
youssefgame39-star <youssefgame39@gmail.com>
2026-08-07 01:03:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const mineflayer = require('mineflayer');
 const http = require('http');
 
-// فتح سيرفر HTTP بسيط لإرضاء Render ومنعه من إغلاق الخدمة
+// فتح سيرفر HTTP لإبقاء الخدمة شغال على Render
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -10,13 +10,11 @@ http.createServer((req, res) => {
   console.log(`Web server listening on port ${port}`);
 });
 
-// إعدادات بوت ماين كرافت
 function createBot() {
   console.log('Connecting to Minecraft server...');
   
   const bot = mineflayer.createBot({
-    host: 'AlWorldA3.aternos.me',
-    port: 61658,
+    host: 'AlWorldA3.aternos.me', // استخدام العنوان الثابت
     username: 'AFK_Bot_Helper'
   });
 
