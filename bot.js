554c84b0674782252bcbbcb1e4fba69688406866
youssefgame39-star <youssefgame39@gmail.com>
2026-08-07 01:08:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 const mineflayer = require('mineflayer');
 const http = require('http');
+const dns = require('dns');
 
-// فتح سيرفر HTTP لمنع Render من إغلاق البوت
+// فتح سيرفر HTTP لإبقاء الخدمة تعمل على Render 24/7
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -10,32 +11,45 @@ http.createServer((req, res) => {
   console.log(`Web server listening on port ${port}`);
 });
 
-function createBot() {
-  console.log('Connecting to Minecraft server...');
+function connectBot() {
+  console.log('Resolving Minecraft server address...');
   
-  const bot = mineflayer.createBot({
-    host: 'AlWorldA3.aternos.me',
-    port: 61658, // اكتب رقم الـ Port الظاهر لك في صفحة Aternos هنا
-    username: 'AFK_Bot_Helper',
-    version: false // يسمح للبوت بالتعرف على إصدار السيرفر تلقائياً
-  });
+  // الاستعلام التلقائي عن الـ IP والـ Port الحالي للسيرفر
+  dns.resolveSrv('_minecraft._tcp.AlWorldA3.aternos.me', (err, addresses) => {
+    let host = 'AlWorldA3.aternos.me';
+    let serverPort = 25565;
 
-  bot.on('login', () => {
-    console.log('>>> Bot has successfully connected to the server! <<<');
-  });
+    if (!err && addresses && addresses.length > 0) {
+      host = addresses[0].name;
+      serverPort = addresses[0].port;
+      console.log(`Connecting via resolved address: ${host}:${serverPort}`);
+    } else {
+      console.log('SRV resolution failed, connecting via default hostname...');
+    }
 
-  bot.on('spawn', () => {
-    console.log('Bot spawned in the world.');
-  });
+    const bot = mineflayer.createBot({
+      host: host,
+      port: serverPort,
+      username: 'AFK_Bot_Helper'
+    });
 
-  bot.on('end', (reason) => {
-    console.log(`Bot disconnected (${reason}). Reconnecting in 15 seconds...`);
-    setTimeout(createBot, 15000);
-  });
+    bot.on('login', () => {
+      console.log('>>> Bot has successfully connected to the server! <<<');
+    });
 
-  bot.on('error', (err) => {
-    console.log('Minecraft Error:', err.message);
+    bot.on('spawn', () => {
+      console.log('Bot spawned in the world.');
+    });
+
+    bot.on('end', (reason) => {
+      console.log(`Bot disconnected (${reason}). Reconnecting in 15 seconds...`);
+      setTimeout(connectBot, 15000);
+    });
+
+    bot.on('error', (err) => {
+      console.log('Minecraft Error:', err.message);
+    });
   });
 }
 
-createBot();
+connectBot();
