f1c42d674e18493a5236514eb30afe0f3e3d9db2
youssefgame39-star <youssefgame39@gmail.com>
2026-08-07 00:51:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,19 @@
 const mineflayer = require('mineflayer');
+const http = require('http');
 
+// فتح سيرفر HTTP بسيط لإرضاء Render ومنعه من إغلاق الخدمة
+const port = process.env.PORT || 3000;
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('AFK Bot is running!\n');
+}).listen(port, () => {
+  console.log(`Web server listening on port ${port}`);
+});
+
+// إعدادات بوت ماين كرافت
 function createBot() {
+  console.log('Connecting to Minecraft server...');
+  
   const bot = mineflayer.createBot({
     host: 'AlWorldA3.aternos.me',
     port: 61658,
@@ -8,16 +21,20 @@ function createBot() {
   });
 
   bot.on('login', () => {
-    console.log('Bot has connected to the server!');
+    console.log('>>> Bot has successfully connected to the server! <<<');
   });
 
-  bot.on('end', () => {
-    console.log('Bot disconnected. Reconnecting in 10 seconds...');
-    setTimeout(createBot, 10000);
+  bot.on('spawn', () => {
+    console.log('Bot spawned in the world.');
+  });
+
+  bot.on('end', (reason) => {
+    console.log(`Bot disconnected (${reason}). Reconnecting in 15 seconds...`);
+    setTimeout(createBot, 15000);
   });
 
   bot.on('error', (err) => {
-    console.log('Error:', err.message);
+    console.log('Minecraft Error:', err.message);
   });
 }
 
